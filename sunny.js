73f0c7d2e8c863d4cf60001f65ee021068aab880
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-02 23:51:27 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,46 @@
+// arrays in js
+// const myarray = [   0,1,2,3,4,5,6       ]
+// const myheroes = ["sunny", "sanaiyla", "kato"]
+// const myarray2 = new Array (1, 2, 3, 4 )
+// console.log(myarray);
+// console.log(myheroes[2]);
+// array methods
+//push adds that value into the code//
+// console.log(myarray.push(7));
+// console.log(myarray.push(88));
+// pop removes the last value//
+// console.log(myarray.pop());
+// unshift m jo b hy wo 0 sy b phly aega//
+// console.log(myarray.unshift(9));
+// shift 0 value ko omit karleta hay
+// console.log(myarray.shift());
+
+// console.log(myarray.includes(99));
+// console.log(myarray.length);
+// indexof tells k value array mn hay ya ni agr hay to as it is value show karay ga otherwise -1 karay ga//
+// console.log(myarray.indexOf(0));
+// console.log(myarray.indexOf(99));
+// join sb values ko join karday ga
+// const newarr = myarray.join()
+// console.log(newarr);
+// console.log(newarr.trim());
+// console.log(newarr.substring());
+// join na srf join karay ga blky iska type of b string karday ga
+// console.log(typeof newarr);
+
+
+
+// slice and splice
+   
+// console.log("A ", myarray);
+// const myn1 = myarray.slice(1,3)
+
+// console.log(myn1);
+
+// console.log("A ", myarray.slice(1,3));
+// slice mn last range incliude nh hte while splice mn hte hay
+// console.log("B", myarray);
+// const myn2 = myarray.splice(1,3)
+// console.log("C ", myarray);
+// console.log(myn2);
+// slice k bd b original array ko kch nh hta but splice  main orig array ka wo hissa splice ho k alg hta hay and array leftover reh jata hay
